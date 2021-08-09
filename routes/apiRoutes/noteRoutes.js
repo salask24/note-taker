053141cf0576827171c90296a5966f36933dc3c6
Kimberly Salas <salask0104@gmail.com>
2021-08-09 09:11:22 -0700

    added note routes and index.js routes

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,34 @@
+const uniqid = require('uniqid');
+const router = require('express').Router();
+const { loadNotes, deleteNoteById, saveNewNote, validateNote, loadNotes } = require("../../lib/notes");
+
+router.get('/notes', (req, res) => {
+    const notes = loadNotes();
+    res.json(notes);
+})
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = uniqid();
+
+    // add note to the db.json file
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note does not have a title and body.');
+    } else {
+        const notes = loadNotes();
+        const note = saveNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const notes = loadNotes();
+    if (req.params.id) {
+        deleteNoteById(req.params.id, notes);
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+module.exports = router;
