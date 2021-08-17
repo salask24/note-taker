e33db883257bec37420498c3eaf9a6809769134b
Kimberly Salas <salask0104@gmail.com>
2021-08-16 19:52:18 -0700

    added to html routes and server js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+// PORT shoudl ALWAYS come before middleware
+const PORT = process.env.PORT || 3001;
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -13,7 +16,6 @@ app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
